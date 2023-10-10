8bc4f135e94d51f09618baa94ca0f43a8e5cd945
Pradipa Rasendrya <pradiparasendrya@gmail.com>
2023-10-10 15:26:57 +0800

    built basework, no patches or functions in place yet

---
+++ LocoOwnership/Main.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Reflection;
+using HarmonyLib;
+using UnityModManagerNet;
+
+namespace LocoOwnership
+{
+	public static class Main
+	{
+		public static bool enabled;
+		public static UnityModManager.ModEntry? mod;
+		public static Settings settings = new Settings();
+
+		private static bool Load(UnityModManager.ModEntry modEntry)
+		{
+			Harmony? harmony = null;
+
+			try
+			{
+				harmony = new Harmony(modEntry.Info.Id);
+				harmony.PatchAll(Assembly.GetExecutingAssembly());
+				DebugLog("Attempting patch.");
+
+				mod = modEntry;
+				modEntry.OnGUI = OnGui;
+				modEntry.OnSaveGUI = OnSaveGui;
+			}
+			catch (Exception ex)
+			{
+				modEntry.Logger.LogException($"Failed to load {modEntry.Info.DisplayName}:", ex);
+				harmony?.UnpatchAll(modEntry.Info.Id);
+				return false;
+			}
+
+			return true;
+		}
+
+		static void OnGui(UnityModManager.ModEntry modEntry)
+		{
+			settings.Draw(modEntry);
+		}
+
+		static void OnSaveGui(UnityModManager.ModEntry modEntry)
+		{
+			settings.Save(modEntry);
+		}
+
+		static void DebugLog(string message)
+		{
+			if (settings.isLoggingEnabled)
+				mod?.Logger.Log(message);
+		}
+	}
+}
---
+++ LocoOwnership/Settings.cs
@@ -0,0 +1,23 @@
+using UnityModManagerNet;
+
+namespace LocoOwnership
+{
+	public class Settings : UnityModManager.ModSettings, IDrawable
+	{
+		public readonly string? version = Main.mod?.Info.Version;
+
+		[Draw("Enable logging")]
+		public bool isLoggingEnabled =
+#if DEBUG
+			true;
+#else
+            false;
+#endif
+		public override void Save(UnityModManager.ModEntry entry)
+		{
+			Save(this, entry);
+		}
+
+		public void OnChange() { }
+	}
+}
